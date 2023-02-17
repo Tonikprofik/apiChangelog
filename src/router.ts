454be7c905a8501e0c8620075662e8fee80b6aec
Tonikprofik <izanami369@gmail.com>
2023-02-17 18:34:18 +0100

    validate input handle

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { body, validationResult } from 'express-validator'
+import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
 
@@ -7,23 +9,32 @@ router.get("/product", (req, res) => {
     res.status(200);
     res.json({ message: "helyo /product bro" });
 });
-router.get("/product/:id", () => {});
-router.put("/product/:id", () => {});
-router.post("/product", (req,res) => {});
-router.delete("/product/:id", () => {});
+router.get("/product/:id", () => { });
+router.put("/product/:id", body('name').isString(), (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+});
+
+router.post("/product", (req, res) => { });
+router.delete("/product/:id", () => { });
 
 // update
-router.get("/update", () => {});
-router.get("/update/:id", () => {});
-router.put("/update/:id", () => {});
-router.post("/update", () => {});
-router.delete("/update/:id", () => {});
+router.get("/update", () => { });
+router.get("/update/:id", () => { });
+router.put("/update/:id", body('name'), body('updatesAt'),handleInputErrors, (req, res) => {
+    
+ });
+router.post("/update", () => { });
+router.delete("/update/:id", () => { });
 
 // update point
-router.get("/update-point", () => {});
-router.get("/update-point/:id", () => {});
-router.put("/update-point/:id", () => {});
-router.post("/update-point", () => {});
-router.delete("/update-point/:id", () => {});
+router.get("/update-point", () => { });
+router.get("/update-point/:id", body('name'), body('updatedAt'), handleInputErrors, (req, res) => { });
+router.put("/update-point/:id", () => { });
+router.post("/update-point", () => { });
+router.delete("/update-point/:id", () => { });
 
 export default router;

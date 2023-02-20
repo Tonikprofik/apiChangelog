c4b6059cd5dc9d5effcae0513833de5bee80bbe9
PonikTrofik <izanami369@gmail.com>
2023-02-20 17:58:26 +0100

    handleipunt errs, getcreate products

--- src/router.ts
+++ src/router.ts
@@ -1,55 +1,79 @@
 import { Router } from "express";
-import { body, oneOf, validationResult } from 'express-validator'
+import { body, oneOf, validationResult } from "express-validator";
+import { createProduct, getProducts } from "./handlers/product";
 import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
 
 // product
-router.get("/product", (req, res) => {
-    res.status(200);
-    res.json({ message: "helyo /product bro" });
+router.get("/product", getProducts, (req, res) => {
+  res.status(200);
+  res.json({ message: "helyo /product bro" });
 });
-router.get("/product/:id", () => { });
-router.put("/product/:id", body('name').isString(), (req, res) => {
+router.get("/product/:id", () => {});
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {
     const errors = validationResult(req);
     console.log(errors);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+      return res.status(400).json({ errors: errors.array() });
     }
-});
+  }
+);
 
-router.post("/product", (req, res) => { });
-router.delete("/product/:id", () => { });
+router.post(
+  "/product",
+  body("name").isString(),
+  handleInputErrors,
+  createProduct
+);
+router.delete("/product/:id", () => {});
 
 // update
-router.get("/update", () => { });
-router.get("/update/:id", () => { });
-router.put("/update/:id",
-    body('title').optional(),
-    body('body').optional(), body('status').isIn(['IN_PROGRESS',
-    'SHIPPED', 'DEPRECATED']).optional(),
-    body('version').optional(), handleInputErrors, (req, res) => {
-    
- });
-router.post("/update",
-    body('title').exists().isString(),
-    body('body').exists().isString(),
-    body('productId').exists().isString(),
- () => { });
-router.delete("/update/:id", () => { });
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put(
+  "/update/:id",
+  body("title").optional(),
+  body("body").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(),
+  body("version").optional(),
+  handleInputErrors,
+  (req, res) => {}
+);
+router.post(
+  "/update",
+  body("title").exists().isString(),
+  body("body").exists().isString(),
+  body("productId").exists().isString(),
+  () => {}
+);
+router.delete("/update/:id", () => {});
 
 // update point
-router.get("/update-point", () => { });
-router.get("/update-point/:id",
-    
-     handleInputErrors, (req, res) => { });
-router.put("/update-point/:id",
-    body('name').optional().isString(),
-    body('description').optional().isString(), () => { });
-router.post("/update-point",
-    body('name').isString(),
-    body('description').isString(),
-    body('updateId').exists().isString(), () => { });
-router.delete("/update-point/:id", () => { });
+router.get("/update-point", () => {});
+router.get(
+  "/update-point/:id",
+
+  handleInputErrors,
+  (req, res) => {}
+);
+router.put(
+  "/update-point/:id",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  () => {}
+);
+router.post(
+  "/update-point",
+  body("name").isString(),
+  body("description").isString(),
+  body("updateId").exists().isString(),
+  () => {}
+);
+router.delete("/update-point/:id", () => {});
 
 export default router;

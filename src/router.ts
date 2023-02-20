85287c0d2adf505c246912dec9f972e734ec5d03
PonikTrofik <izanami369@gmail.com>
2023-02-20 20:07:38 +0100

    updatehand

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,7 @@ import {
   getOneProduct,
   getProducts,
 } from "./handlers/product";
+import { deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from "./handlers/update";
 import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
@@ -38,8 +39,8 @@ router.post(
 router.delete("/product/:id", deleteProduct);
 
 // update
-router.get("/update", () => {});
-router.get("/update/:id", () => {});
+router.get("/update", getUpdates);
+router.get("/update/:id", getOneUpdate);
 router.put(
   "/update/:id",
   body("title").optional(),
@@ -47,17 +48,16 @@ router.put(
   body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(),
   body("version").optional(),
   handleInputErrors,
-  (req, res) => {}
+  updateUpdate
 );
 router.post(
   "/update",
   body("title").exists().isString(),
   body("body").exists().isString(),
   body("productId").exists().isString(),
-
-  () => {}
+  createProduct
 );
-router.delete("/update/:id", () => {});
+router.delete("/update/:id", deleteUpdate);
 
 // update point
 router.get("/update-point", () => {});

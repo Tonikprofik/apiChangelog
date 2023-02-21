ea56a503f52c203522a1e3d7427ae4c2d65bb56f
PonikTrofik <izanami369@gmail.com>
2023-02-21 21:45:33 +0100

    config envs

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,33 +1,38 @@
-import prisma from '../db';
-import { comparePassword, createJWT, hashPassword } from '../modules/auth';
+import prisma from "../db";
+import { comparePassword, createJWT, hashPassword } from "../modules/auth";
 
 export const createNewUser = async (req, res) => {
+  try {
     const { username, password } = req.body;
-    
-    const user = await prisma.user.create({
-        data: {
-            username,
-            password: await hashPassword(password)
-        }
-    })
 
-    const token = createJWT(user)
-    res.json({token})
-}
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: await hashPassword(password),
+      },
+    });
+
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
 export const signIn = async (req, res) => {
-    const { username, password } = req.body;
-    const user = await prisma.user.findUnique({
-        where: {
-            username: username
-        }
-    })
+  const { username, password } = req.body;
+  const user = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
 
-    if (!user) return res.status(404).json({ message: 'User not found' })
-    
-    const valid = await comparePassword(password, user.password)
-    if (!valid) return res.status(401).json({ message: 'Invalid credentials' })
-    
-    const token = createJWT(user)
-    res.json({token})
-}
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  const valid = await comparePassword(password, user.password);
+  if (!valid) return res.status(401).json({ message: "Invalid credentials" });
+
+  const token = createJWT(user);
+  res.json({ token });
+};
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,34 @@
-import express from 'express';
-import router from './router'
-import morgan from 'morgan';
-import { protect } from './modules/auth';
-import { createNewUser, signIn } from './handlers/user';
+import express from "express";
+import router from "./router";
+import morgan from "morgan";
+import { protect } from "./modules/auth";
+import { createNewUser, signIn } from "./handlers/user";
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    console.log("heya express");
-    res.status(200)
-    res.json({message: "helyo"})
-})
+app.get("/", (req, res) => {
+  console.log("heya express");
+  res.status(200);
+  res.json({ message: "helyo" });
+});
 
-app.use('/api', protect, router)
+app.use("/api", protect, router);
 
-app.post('/user', createNewUser)
-app.post('/signin', signIn)
+app.post("/user", createNewUser);
+app.post("/signin", signIn);
+
+app.use((err, req, res, next) => {
+  if (err.type === "auth") {
+    res.status(401).json({ message: "unautho" });
+  } else if (err.type === "input") {
+    res.status(400).json({ message: "bad input" });
+  } else {
+    res.status(500).json({ message: "internal server error" });
+  }
+});
 
 export default app;
